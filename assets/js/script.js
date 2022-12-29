711b4eeb31823e40a076357e3149828680299e96
Danny <100567590+dannymac90@users.noreply.github.com>
2022-12-29 19:04:24 +0000

    update js - Add  all variables

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Declare Display Variables
+let displayHomeScore = document.getElementById("score__el--home") 
+let displayAwayScore = document.getElementById("score__el--guest")
+
+// Declare Home Button Variables
+let homeButton1 = document.getElementById("home__btn--1")
+let homeButton1 = document.getElementById("home__btn--2");
+let homeButton1 = document.getElementById("home__btn--3");
+
+// Declare Away Button Variables
+let homeButton1 = document.getElementById("away__btn--1")
+let homeButton1 = document.getElementById("away__btn--2");
+let homeButton1 = document.getElementById("away__btn--3");
+
+// Score variables
+let homeCount = 0
+let awayCount = 0
+
